b808b9551ec03977218d70fde775ff47e832717d
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-03-04 17:17:10 +0100

    commit3

--- wirtualka/crm/ankieta/models.py
+++ wirtualka/crm/ankieta/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Pytanie(models.Model):
+    tekst_pytania=models.CharField(max_length=300)
+    pub_date=models.DateTimeField('data publikacji')
+
+class Odpowiedz(models.Model):
+    pytanie=models.ForeignKey(Pytanie, on_delete=models.CASCADE)
+    tekst_wyboru=models.CharField(max_length=300)
+    glosy=models.IntegerField(default=0)
+        
